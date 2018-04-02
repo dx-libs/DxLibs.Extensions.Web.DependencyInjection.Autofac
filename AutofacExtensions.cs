58d2bcd9fedceb8aa4793d8563fd69aba5138bc7
dx Team <32604377+dx-team@users.noreply.github.com>
2018-04-02 03:33:24 -0300

    Initial commit

---
+++ AutofacExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DxLibs.Extensions.Web.DependencyInjection.Autofac
+{
+    public static class AutofacExtensions
+    {
+        public static IServiceProvider BuildServiceProvider(this IServiceCollection services, Action<IServiceCollection> preBuildProvider = null)
+        {
+            var builderFactory = new ContainerBuilder();
+
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+
+            preBuildProvider?.Invoke(services);
+
+            var container = builder.Build();
+
+            return container.Resolve<IServiceProvider>();
+        }
+    }
+}
